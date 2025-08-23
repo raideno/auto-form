ac2fe9e417c096effe1eef09339c55134abccb59
raideno56 <nadirkichou@hotmail.fr>
2025-08-23 16:17:57 +0200

    chore: cleanup exports

--- lib/index.ts
+++ lib/index.ts
@@ -1,22 +1,10 @@
 import "./styles.css";
 
 export { AutoForm, z_ } from "../src/components/auto-form";
-export type {
-  RenderParams,
-  ControllerParams,
-  ControllerRenderProps,
-  FieldMetadata,
-  CommonMetadata,
-} from "../src/components/auto-form/registry";
+export { useAutoForm } from "../src/components/auto-form/context";
+
 export type {
   AutoFormContextValue,
   FieldConfig,
   FieldGroup,
 } from "../src/components/auto-form/context";
-export type { EnumOption } from "../src/components/auto-form/enhanced-zod";
-export {
-  hasEnhancedOptions,
-  getEnhancedOptions,
-} from "../src/components/auto-form/enhanced-zod";
-export { MetadataRegistry } from "../src/components/auto-form/registry";
-export { useAutoForm } from "../src/components/auto-form/context";
